70548c97371e206cb162be3e14858e39217e9522
tux2bsd <>
2023-11-22 22:04:26 +1300

    adjust readme_usage_check.sh

--- readme_usage_check.sh
+++ readme_usage_check.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-diff <(grep -A99 "^freebsd-update-probe.sh takes no arguments." README.md | grep -B99 ^Version) <(grep -A99 "^freebsd-update-probe.sh takes no arguments." freebsd-update-probe.sh | grep -B99 ^Version)
+diff <(grep -A99 "^* freebsd-update-probe.sh takes no arguments." README.md | grep -B99 ^Version) <(grep -A99 "^* freebsd-update-probe.sh takes no arguments." freebsd-update-probe.sh | grep -B99 ^Version)
 
